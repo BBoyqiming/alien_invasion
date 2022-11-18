6e9635a6134cd27b054c785b57c2a294faf5cf1a
Iori <bboyqiming@gmail.com>
2022-11-18 21:43:55 +0800

    项目更新，实现屏幕中央绘制飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,13 +12,16 @@ class AlienInvasion():
         """初始化游戏并创建游戏资源"""
 
         pygame.init()
+        
+        # 将 Setting 类传给 setting 属性，使其能访问 Setting 类中的配置信息
         self.settings = Settings()
 
         # 创建一个窗口，设置其尺寸，并赋给类属性
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #  此处的 self 表示将 AlienInvasion 实例传入 Ship 中，让 Ship 能够访问 AlienInvasion 类的资源，如 self.screen
+        # 此处的 self 表示将 AlienInvasion 实例传入 Ship 中，
+        # 让 Ship 能够访问 AlienInvasion 类的资源，如 self.screen
         self.ship = Ship(self)
 
     def run_game(self):
@@ -33,7 +36,7 @@ class AlienInvasion():
             # 每次循环时都重绘屏幕
             self.screen.fill(self.settings.bg_color)
             
-            # 调用 Ship 类的 blitme 方法，绘制飞船
+            # 调用 Ship 类的 blitme 方法，每次循环时绘制飞船
             self.ship.blitme()
 
             # 让最近绘制的屏幕可见，在 while 循环的作用下，屏幕会一直刷新
--- ship.py
+++ ship.py
@@ -8,10 +8,10 @@ class Ship:
     def __init__(self, ai_game):
         """初始化飞船并设置其初始位置"""
 
-        # 将类 AlienInvasion 的 screen 属性赋给了 Ship 类的 screen 属性
+        # ai_game 的参数名含义是此处用于传入 AlienInvasion 类
+        # 将 AlienInvasion 类的 screen 属性赋给了 Ship 类的 screen 属性
+        # 即让 Ship 的 screen 属性指向了 pygame 绘制出的屏幕对象
         self.screen = ai_game.screen
-
-
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
@@ -19,6 +19,7 @@ class Ship:
         self.rect = self.image.get_rect()
 
         # 对于每艘新飞船，都将其放置在屏幕底部的中央
+        # 让飞船外接矩形底部中间的位置等于屏幕底部中间的位置
         self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
